99be3f37b8a0dc72b8bc85393c9b7effe8efb101
heroineworshiper <broadcast@earthling.net>
2019-10-08 21:18:57 -0700

    x

---
+++ thirdparty/pulseaudio-13.0/PulseAudioConfig.cmake
@@ -0,0 +1,9 @@
+set(PULSEAUDIO_FOUND TRUE)
+
+set(PULSEAUDIO_VERSION_MAJOR 13)
+set(PULSEAUDIO_VERSION_MINOR 0)
+set(PULSEAUDIO_VERSION 13.0)
+set(PULSEAUDIO_VERSION_STRING "13.0")
+
+find_path(PULSEAUDIO_INCLUDE_DIR pulse/pulseaudio.h HINTS "/usr/local/include")
+find_library(PULSEAUDIO_LIBRARY NAMES pulse libpulse HINTS "/usr/local/lib")
